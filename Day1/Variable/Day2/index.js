311cba4299ae06ec052fb20af94fd235bb9851c9
komal hargunani <komal.hargunani@synerzip.com>
2023-05-25 17:20:30 +0530

    day1 assignment

---
+++ Day1/Variable/Day2/index.js
@@ -0,0 +1,15 @@
+let marks = [75,45,100,55,44]
+const name = ['A', 'B', 'C', 'D']
+const mixed = ['Str', 89, [19,20]]
+
+const arr = new Array(23,123,21,'orange')
+
+console.log(marks);
+console.log(mixed);
+console.log(arr);
+
+let val = mixed.indexOf(89);
+console.log(val);
+
+marks.push(100);
+console.log(marks);
--- JavaScript/Variable/Day1/date.js
+++ JavaScript/Variable/Day1/date.js
@@ -1,2 +1,3 @@
+//js method used to print today's date and time
 const date = new Date();
 console.log(date); 
