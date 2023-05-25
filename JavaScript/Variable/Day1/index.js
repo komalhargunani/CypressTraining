f85ef19deefafdbe2266ac98e3727bbc5e04266e
komal hargunani <komal.hargunani@synerzip.com>
2023-05-25 12:00:10 +0530

    Added my project

---
+++ JavaScript/Variable/Day1/index.js
@@ -0,0 +1,15 @@
+const date = new Date();
+console.log(date); 
+/*
+console.log("This is javascript basoc training")
+console.error("This is error");
+console.warn("This is warning");
+console.table({Vrushalee:"Training"});
+alert("This is alert")
+function f(){
+    var a =10;
+    console.log(a)
+}
+f();
+console.log(a)
+*/
